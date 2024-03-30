f7792979e2aae866368f14ac71a720ce4a533592
leguan <longboard.noah@gmail.com>
2024-03-30 15:51:14 +0100

    finished basic invsee functionality

--- src/main/java/at/noahb/invsee/listener/InventoryListener.java
+++ src/main/java/at/noahb/invsee/listener/InventoryListener.java
@@ -1,21 +1,48 @@
 package at.noahb.invsee.listener;
 
 import at.noahb.invsee.Invsee;
-import io.papermc.paper.event.player.PlayerInventorySlotChangeEvent;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.event.inventory.InventoryAction;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
 
 public record InventoryListener(Invsee instance) implements Listener {
 
     @EventHandler
-    public void onInvClose(InventoryCloseEvent event) {
+    public void onInventoryClose(InventoryCloseEvent event) {
         instance.getSessionManager().removeSubscriberFromSession(event.getPlayer());
     }
 
     @EventHandler
-    public void onInventory(PlayerInventorySlotChangeEvent event) {
-        System.out.println("Abc");
-        instance.getSessionManager().updateContent(event.getPlayer());
+    public void onInventoryClick(InventoryClickEvent event) {
+        if (event.getAction() == InventoryAction.NOTHING) {
+            return;
+        }
+        System.out.println("InvClick");
+        handle(event.getWhoClicked());
+    }
+
+    @EventHandler
+    public void onPickupItem(EntityPickupItemEvent event) {
+        System.out.println("pickup");
+        handle(event.getEntity());
+    }
+
+    @EventHandler
+    public void onDrag(InventoryDragEvent event) {
+        System.out.println("drag");
+        handle(event.getWhoClicked());
+    }
+
+    private void handle(LivingEntity entity) {
+        if (entity instanceof Player player) {
+
+            player.getScheduler().run(instance, scheduledTask -> instance.getSessionManager().updateContent(player), null);
+        }
     }
 }
